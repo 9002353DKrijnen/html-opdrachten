398f5101d441a23ec2150bfd64bbaca48d3617b4
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-10-21 11:50:35 +0200

    op 6.2

---
+++ Javascript/javascript 7.1/style.js
@@ -0,0 +1,12 @@
+function calculate ( par1,par2 ) {
+    	let resulte = (par1 + par2);
+        console.log(resulte);
+}
+function multiplyByFive (par ) {
+    let resulte = (par * 5);
+    console.log(resulte);
+}
+function calculateMinutes (par) {
+    let resulte = (par / 60);
+    console.log(resulte);
+}
