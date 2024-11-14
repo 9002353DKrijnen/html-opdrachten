d3aa5f39ee453314fc870810a9fd07484277595a
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-11-14 15:08:56 +0100

    8.2

---
+++ Javascript/opdracht 8.2/script.js
@@ -0,0 +1,51 @@
+
+function keer() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    if (num1 <= 0 || num2 <= 0) {
+    document.getElementById("result").innerText = "invoer onjuist"      
+    } else {
+    const result = num1 * num2;
+    document.getElementById("result").innerText = "Resultaat: " + result;
+}
+}
+function delen() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    if (num1 <= 0 || num2 <= 0) {
+        document.getElementById("result").innerText = "invoer onjuist"      
+        } else {
+            if (num2 !== 0) {
+                const result = num1 / num2;
+                document.getElementById("result").innerText = "Resultaat: " + result;
+            } else {
+                document.getElementById("result").innerText = "Kan niet door 0 delen";
+            }
+
+}
+}
+function plus() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+
+   if (num1 <= 0 || num2 <= 0) {
+        document.getElementById("result").innerText = "invoer onjuist"      
+     }  else {  
+            const result = num1 + num2;
+            document.getElementById("result").innerText = "Resultaat: " + result;
+             }
+}
+
+function min() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    if (num1 <= 0 || num2 <= 0) {
+        document.getElementById("result").innerText = "invoer onjuist"      
+
+        
+    } else {
+        const result = num1 - num2;
+        document.getElementById("result").innerText = "Resultaat: " + result;
+    }
+        
+    }
