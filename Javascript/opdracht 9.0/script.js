999fa59dbf654ac21253720ab2d7eecfd41a3e49
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-11-19 08:45:52 +0100

    start 9

---
+++ Javascript/opdracht 9.0/script.js
@@ -0,0 +1,15 @@
+var coockie = document.getElementById("coockiewall");
+var btn = document.getElementById("koekies");
+var span = document.getElementById("close");
+
+btn.onclick = function(){
+ coockie.style.display = "block";
+}
+
+span.onclick = function()
+{
+ coockie.style.display = "none";
+}
+
+
+
