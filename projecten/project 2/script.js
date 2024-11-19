82c3caf3c4c2c2d39df183b198d85cfcb276eee6
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-11-19 15:54:03 +0100

    in JS

---
+++ projecten/project 2/script.js
@@ -0,0 +1,9 @@
+function resetStyles() {
+    var topSection = document.querySelector(".top-section");
+        topSection.style.background = "initial";
+        topSection.style.cursor = "initial";
+        topSection.style.color = "initial";
+        topSection.style.zIndex = "auto";
+        topSection.style.filter = "none";
+    
+}
