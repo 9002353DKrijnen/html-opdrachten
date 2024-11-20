7101a11f76bf239d602f35a3b3f983870de14141
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-11-20 10:28:41 +0100

    op1

---
+++ PHP/Opdracht 1/helloworld.php
@@ -0,0 +1,11 @@
+
+<?php
+    echo "<h1>hello <b>world</h1></b>"; #Dit is commentaar
+    echo "<p>hello <b>world</b></p>"; #Dit is commentaar
+$x = "25:";
+$y = "Leeg";
+echo "$x $y";
+if ($x == "25") {
+ echo "<hr>";}
+else { echo "<br>";}
+?>
