c1048a0ac1b20f58e96a7cf84f548c645c0999ab
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-11-24 10:34:02 +0100

    Create opdracht 2.php

---
+++ PHP/Opdracht 2/opdracht 2.php
@@ -0,0 +1,22 @@
+<?php
+
+//  Een voorbeeld van een echo en een print
+echo "<h1>Hallo wereld,</h1>";
+echo "Ik zit op een stoel ";
+
+// Voorbeeld van een variabele in PHP.
+$schoolnaam = "Techniek College Rotterdam !";
+
+// echo
+echo "op het $schoolnaam";
+
+//  h1 met een p zonder p
+echo "<h1>Hallo wereld,</h1>";
+echo "Ik ben Damiën en ";
+
+// Variable
+$x = "Dit is mijn 2de php opdracht !";
+
+// variable opgeroepen
+echo "$x";
+?>
