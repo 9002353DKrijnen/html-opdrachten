50fe97e16238c12924ece1e6f4c4b498eeae50fb
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-11-25 14:25:23 +0100

    INIT opdr 2

--- Javascript/opdracht 9.2/app.js
+++ Javascript/opdracht 9.2/app.js
@@ -1,14 +1,10 @@
-
 var emojiClosed = document.querySelector(".emoji.closed.active");
 var emojiOpen = document.querySelector(".emoji.open");
-
     emojiClosed.onclick = function() {
         emojiClosed.style.display = "none";  
         emojiOpen.style.display = "block";  
     }
-
     emojiOpen.onclick = function() {
         emojiOpen.style.display = "none";   
         emojiClosed.style.display = "block"; 
     }
-
