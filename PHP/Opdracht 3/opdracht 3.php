db783398d6d1bc2148cbd3599062bc1efe46c2d4
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-11-28 15:25:58 +0100

    Create opdracht 3.php

---
+++ PHP/Opdracht 3/opdracht 3.php
@@ -0,0 +1,20 @@
+<?php
+
+$hallo = "Hallo";
+$hoi = "Hallo";
+$hello = "Hallo";
+$getalmeteenprint = "7.000.000";
+
+echo "$hallo";
+echo "<br>"
+echo "$hoi";
+echo "<br>"
+
+print "$hello";
+echo "<br>"
+
+print "$getalmeteenprint";
+echo "<br>"
+
+
+?>
