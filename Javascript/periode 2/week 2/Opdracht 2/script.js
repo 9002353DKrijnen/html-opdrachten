4343b7cce5b2bef72b0a484e94c2f6c6fedf32dc
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-12-01 19:06:20 +0100

    op2

--- Javascript/periode 2/week 2/Opdracht 2/script.js
+++ Javascript/periode 2/week 2/Opdracht 2/script.js
@@ -1,10 +1,11 @@
-
-//functie aanmaken
-// Waarde ophalen bij product invoer
-//Uitvoer met console.log
-function addProduct()
-{
-    var product = document.getElementById("productInvoer").value;
-    console.log("Ingevoerde product:" + product);
+// Opdracht 2, de animatie duurt 4 seconden, ik had eerst klakkeloos timeout() neergezet zonder opening dus ja.....
+function Startanimatie(){
+ setTimeout(function(){
+document.getElementById("animation").style.animation = "JsAnimatielinks 1s infinite";
+ }, 0);   
+ setTimeout(function(){
+ document.getElementById("animation").style.animation = "JSanimatierechts 1s infinite";
+ }, 1000);
 }
-   
+// Op herhaling 
+setInterval(Startanimatie, 2000)
