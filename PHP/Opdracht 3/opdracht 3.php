f7e3052dcc271232c1e9b6911a5c5194dc981668
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-12-01 17:23:07 +0100

    Opmaak p2 w2 JS

--- PHP/Opdracht 3/opdracht 3.php
+++ PHP/Opdracht 3/opdracht 3.php
@@ -4,14 +4,12 @@ $hallo = "Hallo";
 $hoi = "Hallo";
 $hello = "Hallo";
 $getalmeteenprint = "7.000.000";
-
 echo "$hallo";
 echo "<br>"
 echo "$hoi";
 echo "<br>"
-
 print "$hello";
-echo "<br>"
+echo "<br>";
 
 print "$getalmeteenprint";
 echo "<br>"
