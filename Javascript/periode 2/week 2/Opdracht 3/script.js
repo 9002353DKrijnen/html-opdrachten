44fce9823d4cb3471a014c05bf09f2b2e02d93a2
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-12-03 16:51:55 +0100

    Opdracht 3

--- Javascript/periode 2/week 2/Opdracht 3/script.js
+++ Javascript/periode 2/week 2/Opdracht 3/script.js
@@ -1,10 +1,27 @@
-
-//functie aanmaken
-// Waarde ophalen bij product invoer
-//Uitvoer met console.log
-function addProduct()
-{
-    var product = document.getElementById("productInvoer").value;
-    console.log("Ingevoerde product:" + product);
+// Opdracht 3
+function Startanimatie(){
+ setTimeout(function(){
+document.getElementById("animation").style.animation = "JsAnimatielinks 1s infinite";
+ }, 0);   
+ setTimeout(function(){
+ document.getElementById("animation").style.animation = "JSanimatierechts 1s infinite";
+ }, 1000);
 }
-   
+// Op herhaling 
+setInterval(Startanimatie, 2000)
+
+// Functie om willekeurige kleur te genereren
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+// aanroepen 
+document.getElementById("animation").addEventListener('click', function() {
+    this.style.backgroundColor = getRandomColor();
+  });
+
+
