106d6f94a8596a4597a8c21f1abb609f95c836dc
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-12-09 09:33:12 +0100

    Opstel opdracht 3

---
+++ projecten/project 2/Opdracht 3/script.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+
+var btn = document.getElementById("btn");
+var form = document.querySelector(".loginForm");
+btn.onclick = function(){
+    event.preventDefault();
+    var username = form.userid.value;
+    var password = form.pwd.value;
+    if (username === "admin" && password === "admin") {
+        window.location.href = "./overzicht.html"; 
+            return true; 
+
+         
+
+
+        } 
+        
+        else if (username === "Mario" && password === "Mario2") {
+                window.location.href = "./rekening_mario.html"; 
+                    return true; 
+                }
+                
+    else {
+                    alert("Onjuiste gebruikersnaam of wachtwoord");
+                    return false; 
+                }
+
+        }
+            
+       
+        }
+
