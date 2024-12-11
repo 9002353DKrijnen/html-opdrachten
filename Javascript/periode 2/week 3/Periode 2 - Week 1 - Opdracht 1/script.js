1c5b6220b2ad31c92b266bb7442ffa6829294e86
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-12-11 11:50:46 +0100

    up

--- Javascript/periode 2/week 3/Periode 2 - Week 1 - Opdracht 1/script.js
+++ Javascript/periode 2/week 3/Periode 2 - Week 1 - Opdracht 1/script.js
@@ -1,13 +1,17 @@
-window.onload = function(){
-    var btn = document.getElementById("button");
-    var modal = document.getElementsByClassName("cookiewall")[0];
-    var background = document.getElementsByClassName("background")[0];
-   btn.onclick = function(){
-    modal.style.display = "none"; 
-    background.style.display = "none"; 
-   }
+// bron : https://www.w3schools.com/js/tryit.asp?filename=tryjs_loop_for_om1
 
-
-
+// Auteur: damien krijnen 
+// Opdracht w3 1
+// waardes van het mandje
+const winkelmandje = ["Appels","Boeken","Bananen","Gehakt"];
+// kwantiteit definieren met .length 
+let quantity = winkelmandje.length;
+// variabel definieren
+let i, len, text;
+for (i = 0, len = winkelmandje.length, text = ""; i < len; i++)
+    // text + winkelmandje 
+{
+    text += winkelmandje[i] + "<br>"
 }
-
+// waarde schrijven naar 
+document.getElementById("winkelmandje").innerHTML =      "Er zitten " + quantity + " producten in uw winkelmand:<br>" + text;
