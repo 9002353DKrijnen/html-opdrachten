f17f0a67fc5748036cc802993ff7641747eb06e5
LBC187-winniedos <9002353@student.tcrmbo.nl>
2024-12-30 15:43:49 +0100

    Opstart en opdracht 7.1

---
+++ PHP/Opdracht 6/opdracht 7.2.php
@@ -0,0 +1,26 @@
+<?php
+
+//Opdracht 2: Maak een calulcator met de gebruiksmogelijkheden: 2 getallen waarin je kan optellen, aftrekken, vermeningvuldigen en kan delen. 
+//Opdracht 3: Maak een script waarbij het mogelijk is om de achtergrondkleur van de pagina in te stellen. Met een aantal radio buttons kan er 1 kleur worden gekozen. 
+//Opdracht 4: Maak een Script met een formulier waarmee je korting op een geldbedrag kunt berekenen, de gebruiker moet een geldbedrag en een kortingspercentage kunnen invoeren. 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>opdracht 7.2</title>
+</head>
+<body>
+    <form name="calculator" action="">
+        <label for=""></label>
+    <input type="text" >
+
+
+
+
+
+    </form>
+
+</body>
+</html>
