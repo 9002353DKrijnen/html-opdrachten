b83ae8eb8caef7783d5b2dff61b0c6e5fb776258
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-01-30 14:08:57 +0100

    Opdracht 9.1

--- PHP/Opdracht 9 (SELECT)/Opdracht 9.1.php
+++ PHP/Opdracht 9 (SELECT)/Opdracht 9.1.php
@@ -1,3 +1,33 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opdracht 9.1</title>
+</head>
+<body>
+    <style>
+table{
+    border-collapse: collapse;
+    width: 100%;
+    color: #588c7e;
+    font-family: d;
+    font-size: 25px;
+    text-align: left;
+}
+th{
+    background-color: #588c7e;
+    color: white;
+}
+td{
+    border: solid 2px black;
+    text-align: center;
+}
+
+    </style>
+</body>
+</html>
 <?php
 
 /*
@@ -7,12 +37,21 @@ De bedoeling van opdracht 9.1 is verder ingaan op de koppeling tussen php en een
 Opdracht 9.1: Voer alle stappen uit die beschreven staan in 9.3.
 */
 // Opdracht SELECT
-$db = new PDO('mysql:host=localhost;dbname=fietsenmaker','root','');
-
-$query = $db->prepare("SELECT * FROM fietsen");
-$query->execute();
-$result = $query->fetchAll(PDO::FETCH_ASSOC);
-foreach($result as $data){
-    echo $data["merk"] . "";
+try {
+    $db = new PDO('mysql:host=localhost;dbname=fietsenmaker', 'root', '');
+    $query = $db->prepare("SELECT * FROM fietsen");
+    $query->execute();
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+    echo "<table>";
+    foreach ($result as $data) {
+    echo "<tr>";
+        echo "<td>" . $data["merk"] . "</td>";
+        echo "<td>" . $data["type"] . "</td>";
+        echo "<td>  &euro;"   . number_format($data["prijs"],2,",",".")  . "</td>"."<br>";
+    echo "</tr>";
+    }
+    echo "</table>";
+} catch (PDOException $error) {
+    die("Error: " . $error->getMessage());
 }
 ?>
