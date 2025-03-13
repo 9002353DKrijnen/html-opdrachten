b25705f8871449986acc7929bd957ddc71cc005b
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-03-13 20:49:56 +0100

    CONNECt Opstel

--- PHP/periode 3/Opdracht 13/connect.php
+++ PHP/periode 3/Opdracht 13/connect.php
@@ -1,20 +1,25 @@
 <?php
-function ConnectDb(){
-    /* gegevens van de database */
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "cijfers";
+// database conncectie maken
+// Met dit bestand kan ik de database connecie maken
+// Dit bestand wordt gebruikt door alle andere php bestanden
+$host = "localhost";
+// gebruiker
+$user = "root";
+// wachtwoord
+$pass = "";
+// database naam
+$db = "studenten_login";
 
-    try {
-        /* Database verbinden middel van PDO */
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        return $conn;
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
-    
-}
+// definieer (optioneel) een DSN voor de database
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+
+/* Dit is de standaard optie array die we meegeven aan de PDO constructor.
+Dit is om fouten te tonen als er iets fout gaat. Bijvoorbeeld een fout in de SQL query */
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
 ?>
--- PHP/periode 3/Opdracht 13/functions.php
+++ PHP/periode 3/Opdracht 13/functions.php
@@ -1,20 +1,5 @@
 <?php
-function connectDB(){
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "studenten_login";
 
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        // set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        return $conn;
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
-}
 function registerForm(){  
     $formUserReg = "
     <form action='register.php' method='post'>
