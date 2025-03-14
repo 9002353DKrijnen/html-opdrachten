1b9368022130043da5dbeb1b03fa58dca94075cb
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-03-14 10:39:10 +0100

    Create editpasswd.php

---
+++ PHP/periode 3/Opdracht 13/editpasswd.php
@@ -0,0 +1,50 @@
+<?php
+// database gegevens met de basis gegevens
+require "connect.php";
+
+// sessie starten met session_start();
+session_start();
+
+// try en catch om fouten te voorkomen, met foutverwerking
+try {
+    // maak verbinding met de database
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+
+    // Haal gebruiker uit de database
+    $query = $conn->prepare("SELECT username FROM users");
+
+    // voer query uit
+    $users = $query->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    // errorverwerking
+    die("Error:" . $e->getMessage());
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>editpasswd</title>
+</head>
+
+<body>
+    <
+        <h2>Gebruikers wachtwoorden wijzigen</h2>
+        <form action="editpasswd.php" method="post">
+            <div>
+                <select name="user" id="user" required>
+                    <?php foreach ($users as $user): ?>
+                        <option value="<?= htmlspecialchars($user['username']); ?>"><?= htmlspecialchars($user['username']); ?></option>
+                    <?php endforeach; ?>
+                    ?>
+
+                </select>
+            </div>
+            <div>
+
+
+</body>
+
+</html>
