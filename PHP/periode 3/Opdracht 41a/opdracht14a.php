6b6784da62786af166bb1e5ae38b7300f2a8b315
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-03-20 15:55:05 +0100

    Opdracht 14a start

---
+++ PHP/periode 3/Opdracht 41a/opdracht14a.php
@@ -0,0 +1,20 @@
+<?php
+ /* Opdracht 14  
+ Damien Krijnen
+ Gastenboek opdracht: 
+ 
+ Maak met behulp van het boek de opdracht 6 van hoofdstuk 9. 
+
+Video-instructie :
+
+Les - PROGR1 JLSVSOD1C,JLSVSOD1B,JLSVSOD1A Programmeren 1-20250313_105912-Opname van vergadering 1.mp4
+
+Koppelingen naar een externe site.
+
+
+Test alles goed door.
+
+Gebruik het boek en de eerder gemaakte opdrachten ter referentie.
+
+Upload hier de code en een korte videopresentatie */
+?>
