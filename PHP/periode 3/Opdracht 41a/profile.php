363732ba8bd757113a1b501efa854d3339bc4dd3
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-03-20 16:48:52 +0100

    Create profile.php

---
+++ PHP/periode 3/Opdracht 41a/profile.php
@@ -0,0 +1,14 @@
+<?php
+$database =  ['default' => ['dbname' => 'bieren',
+    'tables' => ['bier', 'brouwer', 'brouwer2', 'kroeg', 'land', 'schenkt']
+],
+Hel
+
+
+
+
+
+
+];
+$username = "root";
+$password = "";
