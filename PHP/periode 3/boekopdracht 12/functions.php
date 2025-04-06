6fb3f3dccc20ab477dc1c1717c9719cadda3fef4
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-04-06 14:40:31 +0200

    opdracht 2, aparte functies
    Gerealiseerd met strpdos(), simpelweg een worod zoeken in een string, deze returnen als associative array

--- PHP/periode 3/boekopdracht 12/functions.php
+++ PHP/periode 3/boekopdracht 12/functions.php
@@ -10,3 +10,60 @@ function generateRandomPassword($length = 10)
     // $length is de lengte
     return substr(str_shuffle($allowedChars), 0, $length);
 }
+
+function getUseragent(){
+    $fulldata = $_SERVER['HTTP_USER_AGENT'];
+    return $fulldata;
+}
+
+function Filteredfulldata($fulldata){
+
+// bepaal bestuuringssysteem
+// strpos() zoekt naar een woord in een string
+    switch(true){
+        case strpos($fulldata, 'Windows') !== false:
+            $computerdata = 'Windows';
+            break;
+
+        case strpos($fulldata, 'Linux') !== false:
+            $computerdata = 'Linux';
+            break;
+
+        case strpos($fulldata, 'Mac') !== false:
+            $computerdata = 'Mac';
+            break;
+
+            default:
+            $computerdata = 'onbekende bestuuringssysteem';
+            break;
+    }
+    
+    // bepaal browser
+    switch(true){
+        case strpos($fulldata, 'Chrome') !== false:
+            $browserdata = 'Chrome';
+            break;
+
+
+        case strpos($fulldata, 'Firefox') !== false:
+            $browserdata = 'Firefox';
+            break;
+
+
+        case strpos($fulldata, 'Edge') !== false:
+            $browserdata = 'Edge';
+            break;
+
+
+        case strpos($fulldata, 'Safari') !== false:
+            $browserdata = 'Safari';
+            break;
+
+
+            default:
+            $browserdata = 'onbekende browser';
+            break;
+    }
+    // stop de returns in een array die kunnen worden opgeroepen
+    return array ('computerdata' => $computerdata, 'browserdata' => $browserdata);
+}
--- PHP/periode 3/boekopdracht 12/super.php
+++ PHP/periode 3/boekopdracht 12/super.php
@@ -7,6 +7,17 @@
 // opdracht willekeurige wachtwoord van 10 tekens:  
 
 include 'functions.php';
-
+$fulldata = getUseragent();
+$filteredData = Filteredfulldata($fulldata);
 echo "uw wachtwoord =" . generateRandomPassword(10);
+echo "<br> Volledige gegevens van uw browser =" . getUseragent($fulldata);
+echo "<br> uw bestuuringssysteem =" . $filteredData['computerdata'];
+echo "<br> uw browser =" . $filteredData['browserdata'];
+echo "<style>body{background-color: #FFC0CB;
+display: flex;
+justify-content: center;
+align-items: center;}
+
+
+</style>";
 ?>
