1c483cf958e44f8bca1ca5c1f758fa84391f9394
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-06-09 11:55:36 +0200

    18c

---
+++ PHP/Periode 4/opdracht 18c/functions.php
@@ -0,0 +1,55 @@
+<?php
+function dbSelect($database = 'default')
+{
+    require 'profile.php';
+    // stel $db in zodat we de juiste database kunnen gebruiken 
+
+    $dsn = "mysql:host=$host;dbname={$database}";
+
+
+    // Maak verbinding met database
+    try {
+        $conn = new PDO($dsn, $username, $password, $options);
+        return $conn;
+    } catch (PDOException $e) {
+        die("Connection failed: " . $e->getMessage());
+    }
+}
+  /* Casus 3: Statistiekensysteem
+Maak casus 3 het statistiekensysteem van paragraaf 10.2 Casussen
+
+Doelstelling: Ontwikkel een statistiekensysteem waarbij de gegevens van bezoekers worden opgeslagen in een database en de beheerder deze gegevens kan filteren op maand en land.
+
+Video-instructie case  3 statistiekensysteem en imagemap voor het project Escape room:
+Les - PROGR1 JLSVSOD1C,JLSVSOD1B,JLSVSOD1A Programmeren 1-20250605_105943-Opname van vergadering.mp4
+Koppelingen naar een externe site.
+
+Vereiste gegevens:
+
+    Land (het land mag verzonnen worden)
+    IP-adres (dit mag het lokale adres zijn of verzonnen worden. Om het lokale ipadres te achterhalen gebruik je:
+    $ipadres = gethostbyname(gethostname());
+    Provider
+    Browser
+    Datum/tijd van het bezoek
+    Website waarvandaan de bezoeker is gekomen
+
+Functionaliteiten:
+
+    Opslag van de bezoekersgegevens in een database.
+    Filteroptie voor de beheerder om gegevens op maand en land te filteren.
+    Minimaal 100 records in de database.
+
+Stappenplan
+
+    Database Ontwerp
+        Maak een database genaamd statistiekensysteem.
+        Maak een tabel genaamd bezoekers met de volgende velden:
+            id (int, primary key, auto-increment)
+            land (varchar(100))
+            ip_adres (varchar(45))
+            provider (varchar(100))
+            browser (varchar(100))
+            datum_tijd (datetime)
+            referer (varchar(255))
+ */
---
+++ PHP/Periode 4/opdracht 18c/profile.php
@@ -0,0 +1,16 @@
+<?php
+// Definieer databases in een apart database bestand. 
+
+// inloggegevens voor localhost
+$username = "root";
+$password = "";
+$host = "localhost";
+
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+];
+
+
+
+?>
