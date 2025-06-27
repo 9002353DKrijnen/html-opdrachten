2b65b03964ce2938e3e9b5b451be55c3d9640b0c
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-06-27 17:32:31 +0200

    setup  emoji converter

--- PHP/Periode 4/Opdracht 18E/functions.php
+++ PHP/Periode 4/Opdracht 18E/functions.php
@@ -1,9 +1,9 @@
 <?php
 
-function getData($id) {
+function getData($id)
+{
 
     connectDataBase('db');
-    
 }
 
 function connectDataBase($dbname)
@@ -17,17 +17,32 @@ function connectDataBase($dbname)
     // We want $conn to be returned on a succesful connection
     // Fist we will use conn and catch to get a report if no connection was established
 
-    try{
+    try {
         $conn = new PDO($dsn, $username, $password, $options);
 
         // return connection upon succesfully connecting 
         return $conn;
-    } catch (PDOException $event){
+    } catch (PDOException $event) {
 
 
         // show error in a stringinterpolation message
         die("Connection failed: {$event->getMessage()}");
-
     }
+}
 
+function emojiReplace($text)
+{
+    $emojimap = [
+        ':)' => "<img src='./user/emoji/smile.png' alt='smile' style='height:0.9em; vertical-align:middle;'>",
+        ':(' => "<img src='./user/emoji/sad.png' alt='sad' style='height:0.9em; vertical-align:middle;'>",
+        ':o' => "<img src='./user/emoji/wow.png' alt='wow' style='height:0.9em; vertical-align:middle;'>",
+
+    ];
+
+    // for every emoji it comes across it will replace it wile an emoji
+    foreach ($emojimap as $emoji => $image) {
+        $text = str_replace($emoji, $image, $text);
+    }
+    // return new $text value 
+    return $text;
 }
