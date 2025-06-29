d54cac71ef345dc6069f9658fbc6d0de08285a48
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-06-29 16:24:13 +0200

    add-idea

---
+++ PHP/Periode 4/Opdracht 18E/add_idea.php
@@ -0,0 +1,76 @@
+<form action="" method="post">
+    <label for="title">titel</label>
+    <input type="text" name="title" required>
+    <label for="idea">Idee</label>
+    <input type="text" name="idea" required>
+    <label for="email">E-mail:</label>
+    <input type="email" name="email" id="email">
+    <button type="submit" name="submit">Verzenden</button>
+
+</form>
+<script>
+    // listen when page is loaded
+    addEventListener("DOMContentLoaded", () => {
+
+        // declare email
+        const email = document.getElementById("email");
+        // declare email value
+        // check if there's an error message, so we do not duplicate it.
+        email.addEventListener("input", () => {
+            let emailError = document.getElementById("emailError");
+            if (emailError) {
+                emailError.remove();
+            }
+
+            // check on change on entered email
+            const emailValue = email.value;
+            if (emailValue === "") {
+                return;
+            }
+
+            if (!emailValue.includes("@") || !emailValue.includes(".")) {
+                let newP = document.createElement("p");
+                newP.id = "emailError";
+                newP.textContent = "email lijkt niet geldig.";
+                newP.style.color = "red";
+                document.body.appendChild(newP);
+            }
+        });
+    });
+</script>
+
+
+<style>
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
+
+<?php
+include "functions.php";
+if (isset($_POST["submit"]) && isset($_POST["title"]) && isset($_POST["idea"])) {
+    $title = $_POST["title"];
+    $idea = $_POST["idea"];
+    $email = isset($_POST["email"]) ? $_POST["email"] : null;
+
+
+    $newVote = "INSERT INTO `votes` (`amount`) VALUES (0);";
+    $statement = $conn->prepare($newVote);
+    $statement->execute();
+
+    // maak de koppeling
+    $conn = connectDataBase('ideeenbus');
+    $sqlquery = "INSERT INTO `visitor_messages` (`titel`, `idea`, `email`, `time`) 
+    VALUES ('$title', '$idea', '$email','NOW()');";
+    // prepare query
+    $statement = $conn->prepare($sqlquery);
+
+    // bindparam to prevent sql injection
+    $statement->bindParam(":title", $title);
+    $statement->bindParam(":idea", $idea);
+    $statement->bindParam(":email", $email);
+    $statement->execute();
+}
+?>
--- PHP/Periode 4/Opdracht 18E/index.php
+++ PHP/Periode 4/Opdracht 18E/index.php
@@ -2,6 +2,7 @@
 include "functions.php";
 ?>
 
+
 <style>
     * {
         padding: 0;
@@ -9,10 +10,19 @@ include "functions.php";
         box-sizing: border-box;
     }
 
+    body {
+        display: flex;
+        flex-direction: column;
+    }
+
     table {
         border: 1px solid black;
         box-shadow: 0 4px 6px rgba(0, 0, 0, 0.3);
         display: inline-block;
+        margin: 10px;
+        padding: 10px;
+        width: fit-content;
+        align-self: center;
     }
 
     table th {
@@ -26,11 +36,42 @@ include "functions.php";
 
     }
 
-   table tr:hover td {
-    background-color: rgba(0,0,0,0.1);
-    
-}
+    table tr:hover td {
+        background-color: rgba(0, 0, 0, 0.1);
+
+
+
+    }
+
+    section {
+        position: relative;
+        padding: 6px 14px;
+        font-family: -apple-system, BlinkMacSystemFont, 'Roboto', sans-serif;
+        border-radius: 6px;
+        border: none;
+        color: white;
+        background: linear-gradient(180deg, #4B91F7 0%, #367AF6 100%);
+        background-origin: padding-box;
+        background-origin: border-box;
+        box-shadow: 0px 0.5px 1.5px rgba(54, 122, 246, 0.25), inset 0px 0.8px 0px -0.25px rgba(255, 255, 255, 0.2);
+        touch-action: manipulation;
+        width: fit-content;
+        transition: transform 0.3s ease-in, box-shadow 0.3s ease-in;
+        align-self: center;
+
+    }
+
+    section:hover {
+        transform: scale(1.3);
+    }
+
+    section a {
+        text-decoration: none;
+        color: white;
+    }
 </style>
+
+
 <?php
 $results = getData("visitor_messages");
 $votes = getData("votes");
@@ -41,13 +82,20 @@ $votemap = [];
 foreach ($votes as $vote) {
     $votemap[$vote['id']] = $vote['amount'];
 }
+
+
 foreach ($results as $result) {
     echo "<tr><th>" . htmlspecialchars($result['titel']) . "</th></tr>";
     echo "<tr><td>" . htmlspecialchars($result['email']) . "</td></tr>";
     echo "<tr><td>" . htmlspecialchars($result['time']) . "</td></tr>";
-    echo "<tr><td>" . htmlspecialchars($votemap[$result['votes_id']]) . "</td></tr>";
+    echo "<tr><td><strong>Aantal stemmen: </strong>" . htmlspecialchars($votemap[$result['votes_id']]) . "</td></tr>";
     echo "<tr><td>" . htmlspecialchars($result['idea']) . "</td></tr>";
 }
 echo "</table>";
-// Resultaat tonen
 ?>
+
+<section>
+    <a href="./add_idea.php">
+        <p>Voeg nieuw idee toe</p>
+    </a>
+</section>
